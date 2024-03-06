79612f99fff62ae0ffb987823d5e917345cc30a8
sayantanm19 <sayantanmaiti@gmail.com>
2024-03-07 00:30:55 +0530

    Added basic exception handling

--- Bash.UnstructuredIO.Client/UnstructuredClient.cs
+++ Bash.UnstructuredIO.Client/UnstructuredClient.cs
@@ -20,31 +20,17 @@ namespace UnstructuredAPI
     public class UnstructuredClient : IUnstructuredClient
     {
         internal readonly HttpClient _client;
+        internal readonly string _url = "general/v0/general";
 
         /// <summary>
         /// Client for interacting with the Unstructured API.
         /// </summary>
-        public UnstructuredClient(string url)
-        {
-            _client = SetupClient(url);
-        }
-
-        public UnstructuredClient(string url, string apiKey = null)
+        public UnstructuredClient(string url, string? apiKey = null)
         {
             _client = SetupClient(url, apiKey);
         }
 
-        private HttpClient SetupClient(string url)
-        {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(url);
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "multipart/form-data");
-
-            return client;
-        }
-
-        private HttpClient SetupClient(string url, string apiKey)
+        private HttpClient SetupClient(string url, string? apiKey)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri(url);
@@ -60,75 +46,121 @@ namespace UnstructuredAPI
         /// <inheritdoc/>
         public async Task<ApiResponse> PartitionAsync(string filePath, CancellationToken cancellationToken)
         {
-            var formContent = new MultipartFormDataContent();
-            ParametersHelper.AddFileFromPath(formContent, filePath);
-            return await ExecutePartitionRequest(formContent, cancellationToken);
+            try
+            {
+                using var formContent = new MultipartFormDataContent();
+                ParametersHelper.AddFileFromPath(formContent, filePath);
+                return await ExecutePartitionRequest(formContent, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+
         }
 
         /// <inheritdoc/>
         public async Task<ApiResponse> PartitionAsync(string filePath, ExtractionOptions options, CancellationToken cancellationToken)
         {
-            using var formContent = new MultipartFormDataContent();
-            ParametersHelper.AddFileFromPath(formContent, filePath);
-            ParametersHelper.AddMappedParameters(formContent, options);
-            return await ExecutePartitionRequest(formContent, cancellationToken);
+            try
+            {
+                using var formContent = new MultipartFormDataContent();
+                ParametersHelper.AddFileFromPath(formContent, filePath);
+                ParametersHelper.AddMappedParameters(formContent, options);
+                return await ExecutePartitionRequest(formContent, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         /// <inheritdoc/>
         public async Task<ApiResponse> PartitionAsync(Stream fileStream, string fileName, CancellationToken cancellationToken)
         {
-            using var formContent = new MultipartFormDataContent();
-            ParametersHelper.AddFileFromStream(formContent, fileStream, fileName);
-            return await ExecutePartitionRequest(formContent, cancellationToken);
+            try
+            {
+                using var formContent = new MultipartFormDataContent();
+                ParametersHelper.AddFileFromStream(formContent, fileStream, fileName);
+                return await ExecutePartitionRequest(formContent, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
 
         /// <inheritdoc/>
         public async Task<ApiResponse> PartitionAsync(Stream fileStream, string fileName, ExtractionOptions options, CancellationToken cancellationToken)
         {
-            using var formContent = new MultipartFormDataContent();
-            ParametersHelper.AddFileFromStream(formContent, fileStream, fileName);
-            ParametersHelper.AddMappedParameters(formContent, options);
-            return await ExecutePartitionRequest(formContent, cancellationToken);
+            try
+            {
+                using var formContent = new MultipartFormDataContent();
+                ParametersHelper.AddFileFromStream(formContent, fileStream, fileName);
+                ParametersHelper.AddMappedParameters(formContent, options);
+                return await ExecutePartitionRequest(formContent, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
         private async Task<ApiResponse> ExecutePartitionRequest(MultipartFormDataContent formContent, CancellationToken cancellationToken)
         {
-            var response = await _client.PostAsync("general/v0/general", formContent, cancellationToken);
-            var contents = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var response = await _client.PostAsync(_url, formContent, cancellationToken);
+                var contents = await response.Content.ReadAsStringAsync();
 
-            if (response.IsSuccessStatusCode)
-            {
-                var deserializedElements = DeserializeResponse<List<Element>>(contents);
-                return new ApiResponse
+                if (response.IsSuccessStatusCode)
                 {
-                    Data = deserializedElements,
-                    StatusCode = HttpStatusCode.OK
-                };
-            }
-            else
-            {
-                if (response.StatusCode == HttpStatusCode.Unauthorized)
-                {
-                    var deserializeError = DeserializeResponse<ErrorDetail>(contents!);
+                    var deserializedElements = DeserializeResponse<List<Element>>(contents);
                     return new ApiResponse
                     {
-                        Message = deserializeError,
-                        StatusCode = HttpStatusCode.Unauthorized
+                        Data = deserializedElements,
+                        StatusCode = HttpStatusCode.OK
                     };
                 }
                 else
                 {
-                    var deserializeError = DeserializeResponse<ErrorDetails>(contents!);
-                    return new ApiResponse
+                    if (response.StatusCode == HttpStatusCode.Unauthorized)
                     {
-                        Message = deserializeError,
-                        StatusCode = response.StatusCode
-                    };
+                        var deserializeError = DeserializeResponse<ErrorDetail>(contents!);
+                        return new ApiResponse
+                        {
+                            Message = deserializeError,
+                            StatusCode = HttpStatusCode.Unauthorized
+                        };
+                    }
+                    else
+                    {
+                        var deserializeError = DeserializeResponse<ErrorDetails>(contents!);
+                        return new ApiResponse
+                        {
+                            Message = deserializeError,
+                            StatusCode = response.StatusCode
+                        };
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
         }
+
         private T DeserializeResponse<T>(string content)
         {
             return JsonSerializer.Deserialize<T>(content)!;
+        }
+
+        private ApiResponse HandleException(Exception ex)
+        {
+            return new ApiResponse
+            {
+                Message = ex.Message,
+                StatusCode = HttpStatusCode.InternalServerError
+            };
         }
     }
 }
